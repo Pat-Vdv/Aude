32df1c0101f4905543dd74948d493c41751d407f
Raphaël JAKSE <raphael.jakse@gmail.com>
2013-10-28 14:22:20 +0100

    last commit was wrong: this is for iterator support in lastest soon-to-be-released firefox

--- js/setIterators.js
+++ js/setIterators.js
@@ -2,12 +2,15 @@ Set.prototype.iterator =  function() {
    for(var i in this.l) {
       yield this.l[i];
    }
-   return;
-}
+};
 
-Set.prototype['@@iterator'] =  function() {
-   for(var i in this.l) {
-      yield {value:this.l[i]};
-   }
-   return;
+try {
+   eval("\
+      Set.prototype['@@iterator'] =  function*() {\
+         for(var i in this.l) {\
+            yield this.l[i];\
+         }\
+      };\
+   ")
 }
+catch(e){}
