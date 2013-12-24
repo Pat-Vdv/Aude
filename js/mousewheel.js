6ff23a2df80bbc6b9f6a06a51e6421ed27315819
Raphaël JAKSE <raphael.jakse@gmail.com>
2013-12-24 14:42:15 +0100

    lint mousewheel.js

--- js/mousewheel.js
+++ js/mousewheel.js
@@ -1,43 +1,46 @@
 /** @license WTFPL <http://www.wtfpl.net/txt/copying/> */
 
+/*jslint indent:4, plusplus:true, browser:true*/
 // thx http://www.adomas.org/javascript-mouse-wheel/ for explanations
 
 var listenMouseWheel;
 
-(function() {
-   "use strict";
-   var scrollFunctions = [];
-   function hasParent(ele, parent) {
-      do {
-         if(ele === parent) {
-            return true;
-         }
-         ele = ele.parentNode;
-      }
-      while(ele);
-      return false;
-   }
+(function () {
+    "use strict";
 
-   function handleWheel(e) { 
-      var delta = (e.wheelDelta ? e.wheelDelta/120 : (e.detail ? -e.detail/3 : 0));
+    var scrollFunctions = [];
 
-      if(delta) {
-         for(var i in scrollFunctions) {
-            if(!scrollFunctions[i][1] || hasParent(e.target, scrollFunctions[i][1])) {
-               scrollFunctions[i][0](e, delta);
+    function hasParent(ele, parent) {
+        do {
+            if (ele === parent) {
+                return true;
             }
-         }
-      }
-   }
+            ele = ele.parentNode;
+        } while (ele);
+        return false;
+    }
 
-   if('onmouseweel' in window)
-      window.onmousewheel = handleWheel;
-   else if('onmousewheel' in document)
-      document.onmousewheel = handleWheel;
-   else
-      window.addEventListener('DOMMouseScroll', handleWheel, false);
-      
-   listenMouseWheel = function(f, target) {
-      scrollFunctions.push([f, target]);
-   }
-})();
+    function handleWheel(e) {
+        var i, len, delta = (e.wheelDelta ? e.wheelDelta / 120 : (e.detail ? -e.detail / 3 : 0));
+
+        if (delta) {
+            for (i = 0, len = scrollFunctions.length; i < len; ++i) {
+                if (!scrollFunctions[i][1] || hasParent(e.target, scrollFunctions[i][1])) {
+                    scrollFunctions[i][0](e, delta);
+                }
+            }
+        }
+    }
+
+    if ('onmouseweel' in window) {
+        window.onmousewheel = handleWheel;
+    } else if ('onmousewheel' in document) {
+        document.onmousewheel = handleWheel;
+    } else {
+        window.addEventListener('DOMMouseScroll', handleWheel, false);
+    }
+
+    listenMouseWheel = function (f, target) {
+        scrollFunctions.push([f, target]);
+    };
+}());
