fd3adb4a4de26060847784b188f8539472f934a8
Raphaël JAKSE <raphael.jakse@gmail.com>
2014-03-05 10:57:21 +0100

    aude.js's interactive shell gets colors and does not exit on null value

--- tests/aude.js
+++ tests/aude.js
@@ -33,6 +33,44 @@
 
 var fs, i, j, p;
 
+// thanks colors.js (BSD license, Copyright (c) 2010 Marak Squires, Alexis Sellier (cloudhead))
+var styles = {
+    //styles
+    'bold' : ['\x1B[1m', '\x1B[22m'],
+    'italic' : ['\x1B[3m', '\x1B[23m'],
+    'underline' : ['\x1B[4m', '\x1B[24m'],
+    'inverse' : ['\x1B[7m', '\x1B[27m'],
+    'strikethrough' : ['\x1B[9m', '\x1B[29m'],
+    //text colors
+    //grayscale
+    'white' : ['\x1B[37m', '\x1B[39m'],
+    'grey' : ['\x1B[90m', '\x1B[39m'],
+    'black' : ['\x1B[30m', '\x1B[39m'],
+    //colors
+    'blue' : ['\x1B[34m', '\x1B[39m'],
+    'cyan' : ['\x1B[36m', '\x1B[39m'],
+    'green' : ['\x1B[32m', '\x1B[39m'],
+    'magenta' : ['\x1B[35m', '\x1B[39m'],
+    'red' : ['\x1B[31m', '\x1B[39m'],
+    'yellow' : ['\x1B[33m', '\x1B[39m'],
+    //background colors
+    //grayscale
+    'whiteBG' : ['\x1B[47m', '\x1B[49m'],
+    'greyBG' : ['\x1B[49;5;8m', '\x1B[49m'],
+    'blackBG' : ['\x1B[40m', '\x1B[49m'],
+    //colors
+    'blueBG' : ['\x1B[44m', '\x1B[49m'],
+    'cyanBG' : ['\x1B[46m', '\x1B[49m'],
+    'greenBG' : ['\x1B[42m', '\x1B[49m'],
+    'magentaBG' : ['\x1B[45m', '\x1B[49m'],
+    'redBG' : ['\x1B[41m', '\x1B[49m'],
+    'yellowBG' : ['\x1B[43m', '\x1B[49m']
+};
+
+function style(s, st) {
+    return styles[st][0] + s + styles[st][1];
+}
+
 try {
     fs = require('fs');
 } catch (e) {
@@ -164,10 +202,29 @@ if (process.argv[2]) {
                 console.log(e.stack);
             }
 
-            if (typeof res === 'object' && typeof res.toString === 'function') {
-                console.log(res.toString());
+            if (res && typeof res === 'object' && typeof res.toString === 'function') {
+                if (res instanceof Set) {
+                    console.log(style(res, 'cyan'));
+                } else {
+                    console.log(res.toString());
+                }
             } else {
-                console.log(res);
+                switch(res) {
+                case undefined:
+                    console.log(style('undefined', 'grey'))
+                    break;
+                case null:
+                    console.log(style('null', 'bold'))
+                    break;
+                default:
+                    if (typeof res === 'number') {
+                        console.log(style(res, 'yellow'))
+                    } else if (typeof res === 'string') {
+                        console.log(style("'" + res.replace(/\'|\\/g, '\\$&') + "'", 'green'));
+                    } else {
+                        console.log(res);
+                    }
+                }
             }
 
             audeString = '';
