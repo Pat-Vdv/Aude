259c3d93f31f1f26b59f7a7940c4240bfd6bd54b
Raphaël JAKSE <raphael.jakse@gmail.com>
2015-05-24 10:32:36 +0200

    use Symbol.iterator to fix aude in recent Firefoxes

--- js/setIterators.js
+++ js/setIterators.js
@@ -1,15 +1,22 @@
-Set.prototype.iterator =  function() {
-   for(var i in this.l) {
-      yield this.l[i];
-   }
+Set.prototype.iterator =  function () {
+    for (var i in this.l) {
+        yield this.l[i];
+    }
 };
 
 try {
-   eval("\
-      Set.prototype['@@iterator'] =  function*() {\
-         for(var i in this.l) {\
-            yield this.l[i];\
-         }\
-      };\
-   ");
-} catch (e){}
+    eval("\
+        (function () {\
+            var symbol;\
+            try { symbol = Symbol.iterator; }\
+            catch (e) { symbol = '@@iterator'; }\
+            Set.prototype[symbol] = function*() {\
+                for(var i in this.l) {\
+                    if (this.l.hasOwnProperty(i)) {\
+                        yield this.l[i];\
+                    }\
+                }\
+            };\
+        })();\
+    ");
+} catch (e) {}
